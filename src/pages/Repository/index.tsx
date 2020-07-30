75070fd7bcc0cb7946038b2d12c3631d99df93b1
Gabriel Machado <gabriel@movitech.com.br>
2020-07-30 17:26:53 -0300

    Finalizada interface de repositories

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 
-import { Header, RepositoryInfo } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -11,11 +12,22 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      console.log(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      console.log(response.data);
+    });
+  }, [params.repository]);
+
   return (
     <>
       <Header>
         <img src={logoImg} alt="Github Explorer" />
-        <Link to="/dashboard">
+        <Link to="/">
           <FiChevronLeft size={16} />
           Voltar
         </Link>
@@ -38,15 +50,26 @@ const Repository: React.FC = () => {
             <span>Stars</span>
           </li>
           <li>
-            <strong>1808</strong>
+            <strong>188</strong>
             <span>Forks</span>
           </li>
           <li>
-            <strong>1808</strong>
-            <span>Issues</span>
+            <strong>10</strong>
+            <span>Issues abertas</span>
           </li>
         </ul>
       </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasdasd">
+          <div>
+            <strong>asdasd</strong>
+            <p>asdasd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -55,6 +55,10 @@ export const RepositoryInfo = styled.section`
     margin-top: 40px;
 
     li {
+      & + li {
+        margin-left: 80px;
+      }
+
       strong {
         display: block;
         font-size: 36px;
@@ -66,6 +70,51 @@ export const RepositoryInfo = styled.section`
         margin-top: 4px;
         color: #6c6c80;
       }
+    }
+  }
+`;
+
+export const Issues = styled.div`
+  margin-top: 80px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.3s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
     }
   }
 `;
